90541528439829ed2eb35599bd0ba85b1fac666e
Marcos Luis Hernández <marcoslhc@gmail.com>
2014-07-27 20:07:38 -0400

    Fixes error not creating new resources
    * Theres a new function that makes the correct POST request
    * Adds more closures to clarify the stream

--- test/test.js
+++ test/test.js
@@ -1,38 +1,48 @@
 var should = require("should");
-var transifex = require("../transifex");
+var transifex = require("../index.js");
 var fs = require('fs');
+var gutil = require('gulp-util');
+var config = require('../config.json');
+var path = require('path')
+var env = config.env
+var options = {
+    host: config.transifex.host,
+    base_path: config.transifex.base_path,
+    user: config.transifex[env].user,
+    password: config.transifex[env].password,
+    project: config.transifex[env].project
+};
 
 describe('Transifex', function(){
   describe('#createClient()', function(){
     
     it('should return an object', function() {
-      var client = transifex.createClient();
+      var client = transifex.createClient(options);
       client.should.be.type('object')
     });
     
   });
   
+  /*
   describe('#path', function() {
-    var client = transifex.createClient({
-      project:'marcoslhc_personal'
-    });
-    
-    it('should return an object', function(){
-      client.paths.should.be.type('object')
-    });
-    
-    it('should return the resources path as string', function(){
-      client.paths.get_resources().should.be.a.String;
-      client.paths.get_resources().should.eql('/api/2/project/marcoslhc_personal/resources/');
-    });
-    
-  });
+      var client = transifex.createClient(options);
+      
+      it('should return an object', function(){
+        client.paths.should.be.type('object')
+      });
+      
+      it('should return the resources path as string', function(){
+        client.paths.get_resources().should.be.a.String;
+        client.paths.get_resources().should.eql('/api/2/project/marcoslhc_personal/resources/');
+      });
+      
+    });*/
+  
   
   describe('#listResources()', function(){
     it('shold return the data', function(done){
-      var client = transifex.createClient({
-        project:'marcoslhc_personal'
-      });
+        console.log(config.transifex[env])
+      var client = transifex.createClient(options);
       client.resources(function(data) {
         data.should.be.an.Array
         done()
@@ -42,14 +52,15 @@ describe('Transifex', function(){
 
   describe('#pushResources()', function() {
     it('should', function(done){
-      var client = transifex.createClient({
-        project:'marcoslhc_personal'
+      var client = transifex.createClient(options);
+      file = new gutil.File({
+          path: path.resolve(__dirname, '../fixture.po'),
+          contents: fs.readFileSync(path.resolve(__dirname, '../fixture.po'))
       });
-      fs.createReadStream('/Users/marcoslhc/Documents/Projects/marcoslhc.github.com/locale/eng/LC_MESSAGES/accounts.po')
-        .pipe(client.push(function(){
+      console.log(file.contents.toString('utf8'))
+      client.push(function() {
           done()
-        })
-      );
+      }).write(file);
       
     })
   });
